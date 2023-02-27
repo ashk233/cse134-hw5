c802052796c85f7b000910d74b3be0ff186a7447
Ashley Kou <43892820+ashk233@users.noreply.github.com>
2023-02-26 20:50:42 -0800

    part 2

---
+++ customdialog.js
@@ -0,0 +1,31 @@
+export function createDialog(type) {
+  const temp = document.getElementById("dialog-temp");
+  let clone = temp.content.cloneNode(true);
+  clone.querySelector("div").id = type;
+  let p = clone.querySelector("p");
+  let modalContent = p.parentNode;
+  let ok = clone.querySelectorAll("button")[0];
+  let cancel = clone.querySelectorAll("button")[1];
+  ok.id = `${type}-ok`;
+  cancel.id = `${type}-cancel`;
+  if (type === "confirm") {
+    // confirm
+    p.textContent = "Do you confirm this?";
+  } else if (type === "prompt") {
+    // prompt
+    p.textContent = "What is your name?";
+    p.appendChild(document.createElement("br"));
+
+    p.appendChild(document.createElement("input"));
+    // modalContent.insertBefore(input, p.nextSibling);
+  } else {
+    // alert
+    cancel.remove();
+  }
+  document.body.appendChild(clone);
+}
+
+export function showDialog(type) {
+  var modal = document.getElementById(type);
+  modal.style.display = "flex";
+}

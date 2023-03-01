c5502b038cd2313312bed24912e6891b21ec95de
Ashley Kou <43892820+ashk233@users.noreply.github.com>
2023-02-28 21:46:27 -0800

    part 2

--- customdialog.js
+++ customdialog.js
@@ -1,4 +1,5 @@
 export function createDialog(type) {
+  let output = document.getElementById("output");
   const temp = document.getElementById("dialog-temp");
   let clone = temp.content.cloneNode(true);
   clone.querySelector("div").id = type;
@@ -6,23 +7,54 @@ export function createDialog(type) {
   let ok = clone.querySelectorAll("button")[0];
   let cancel = clone.querySelectorAll("button")[1];
   ok.id = `${type}-ok`;
-  cancel.id = `${type}-cancel`;
-  if (type === "confirm") {
-    // confirm
-    p.textContent = "Do you confirm this?";
-  } else if (type === "prompt") {
-    // prompt
-    p.textContent = "What is your name?";
-    p.appendChild(document.createElement("br"));
-    p.appendChild(document.createElement("input"));
-  } else {
+  if (type == "alert") {
     // alert
     cancel.remove();
+    ok.addEventListener("click", () => {
+      output.innerHTML = "";
+      let modal = document.getElementById(type);
+      modal.style.display = "none";
+    });
+  } else {
+    cancel.id = `${type}-cancel`;
+    if (type === "confirm") {
+      // confirm
+      p.textContent = "Do you confirm this?";
+      ok.addEventListener("click", () => {
+        let modal = document.getElementById(type);
+        modal.style.display = "none";
+        output.innerHTML = "Confirm result: true";
+      });
+      cancel.addEventListener("click", () => {
+        let modal = document.getElementById(type);
+        modal.style.display = "none";
+        output.innerHTML = "Confirm result: false";
+      });
+    } else if (type === "prompt") {
+      // prompt
+      p.textContent = "What is your name?";
+      p.appendChild(document.createElement("br"));
+      let input = document.createElement("input");
+      p.appendChild(input);
+      ok.addEventListener("click", () => {
+        let modal = document.getElementById(type);
+        modal.style.display = "none";
+        let sanitized = DOMPurify.sanitize(input.value);
+        output.innerHTML = `Prompt result: ${sanitized}`;
+        input.value = "";
+      });
+      cancel.addEventListener("click", () => {
+        let modal = document.getElementById(type);
+        modal.style.display = "none";
+        output.innerHTML = "User didn't enter anything.";
+        input.value = "";
+      });
+    }
   }
   document.body.appendChild(clone);
 }
 
 export function showDialog(type) {
-  var modal = document.getElementById(type);
+  let modal = document.getElementById(type);
   modal.style.display = "flex";
 }

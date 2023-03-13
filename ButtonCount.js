1d765a0e95bf3a52cdd85dfb778d6d0986f1a1a3
Ashley Kou <43892820+ashk233@users.noreply.github.com>
2023-03-12 23:27:36 -0700

    button count

---
+++ ButtonCount.js
@@ -0,0 +1,18 @@
+class ButtonCount extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+
+    const button = document.createElement("button");
+    let count = 0;
+    button.textContent = `Times Clicked: ${count}`;
+    button.addEventListener("click", () => {
+      count++;
+      button.textContent = `Times Clicked: ${count}`;
+    });
+
+    shadow.appendChild(button);
+  }
+}
+
+customElements.define("button-count", ButtonCount);
